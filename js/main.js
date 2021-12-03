f866e32b9d7f69b34ad29fb009f33bcb524f13cf
hmjkim <heathermjkim@gmail.com>
2021-12-03 04:13:00 -0800

    added social icons, styled where to buy section

---
+++ js/main.js
@@ -0,0 +1,52 @@
+jQuery(function($){
+
+    /*
+    |----------------------------------------------------------------
+    | Hide/Show Header
+    |----------------------------------------------------------------
+    */
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('nav').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta) {
+          return;
+        }
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('nav').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('nav').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+        lastScrollTop = st;
+    }
+
+    setInterval(function() {
+        if(didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+});
+// flickity
+
+// var element = document.querySelector(".carousel");
+// var flkty = new Flickity( element, {
+//     cellAlign: 'left',
+//     contain: true,
+// });
